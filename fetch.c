01094fdd38de9c2ed707e20c66dd9347f52c921b
donthurtme1 <malwarecunt@proton.me>
2024-03-23 04:58:03 +0000

    More changes.

--- fetch.c
+++ fetch.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
 #include "ascii.h"
 
 static const char *ascii_distro = arch_ascii;
@@ -63,15 +65,20 @@ char getarg(const char *arg) {
 
 int main(int argc, char *argv[]) {
 	/* Command line arguments. */
-	_Bool set_distro = 0;
+	_Bool set_distro = 0, vertical = 0;
 	const char *colour = "36";
 
 	char c;
 	int i = 0;
 	while ((c = getarg(argv[++i]))) {
-		if (c == 'c') colour = argv[++i];
+		if (c == 'h') printf("help\n"); /* TODO */
+		else if (c == 'c') colour = argv[++i];
 		else if (c == 'd') setdistro(argv[++i]);
+		else if (c == 'v') vertical = 1;
 	}
+
+	struct winsize term_size;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &term_size);
 
 	/* Storage */
 	int total_storage, used_storage, storage_percent;
@@ -93,40 +100,58 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* Print stuff. */
-	printf("\e[%s;1m", colour);
-	printf("%s", space_invader);
-	//printf("\e[B\e[43G"); // Cursor position.
-	/* TODO: make cursor position and text colour dependant on selected ascii art. */
-	/*
-	printf("\e[%s;1m%s\e[0m\e[B\e[43G%s\e[B\e[43G", colour, "Simplefetch", "~~~~~~~~~~~"); // Title.
-	printf(field_format, colour, "Distro", "Arch Linux");
-	printf(field_format, colour, "Window Manager", "dwl");
-	printf(field_format, colour, "Packages", "? - pacman");
-	printf(field_format, colour, "Terminal", "Alacritty");
-	printf(field_format, colour, "Terminal Font", "Hack Nerd Font");
-	printf(field_format, colour, "Shell", "bash 5.2.26");
-	printf(field_format, colour, "CPU", "AMD Ryzen 3 3200G @ 3.6 GHz");
-	printf(field_format, colour, "GPU", "AMD Radeon RX 6600");
-	printf(field_format, colour, "Memory", "16 GiB");
-	printf("\e[%s;1mStorage\e[0m: %d / %d GiB\e[53C\e[19D[%s] (%d%%)\e[B\e[43G", colour, used_storage, total_storage, storage_bar, storage_percent);
-
-	printf("\e[B\e[43G");
-	printf("\e[30m███\e[31m███\e[32m███\e[33m███\e[34m███\e[35m███\e[36m███\e[37m███\e[B\e[43G");
-	printf("\e[90m███\e[91m███\e[92m███\e[93m███\e[94m███\e[95m███\e[96m███\e[97m███\e[B\e[43G");
-	printf("\e[4E");
-	*/
-
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	printf("\e[16A");
 	i = 0;
-	while (output_format[i]) {
-		printf("\e[B\e[28G");
-		printf(output_format[i], colour, output_value[i]);
-		i++;
-	}
+	printf("\e[%s;1m", colour);
+	if (!vertical) {
+		printf("%s", space_invader2);
+		while (output_format[i]) {
+			printf("\e[B\e[28G");
+			printf(output_format[i], colour, output_value[i]);
+			i++;
+		}
+		printf("\e[2B\e[28G");
+		printf("\e[30m███\e[31m███\e[32m███\e[33m███\e[34m███\e[35m███\e[36m███\e[37m███\e[B\e[28G");
+		printf("\e[90m███\e[91m███\e[92m███\e[93m███\e[94m███\e[95m███\e[96m███\e[97m███\e[B\e[28G");
+		printf("\e[E");
+	} else {
+		printf("\e[%dG%s", (term_size.ws_col / 2) - 6, space_invader2);
 
-	printf("\e[2B\e[28G");
-	printf("\e[30m███\e[31m███\e[32m███\e[33m███\e[34m███\e[35m███\e[36m███\e[37m███\e[B\e[28G");
-	printf("\e[90m███\e[91m███\e[92m███\e[93m███\e[94m███\e[95m███\e[96m███\e[97m███\e[B\e[28G");
-	printf("\e[E");
+		printf("\e[2B\e[%dG", (term_size.ws_col / 2) - 12);
+		printf("\e[30m███\e[31m███\e[32m███\e[33m███\e[34m███\e[35m███\e[36m███\e[37m███");
+		printf("\e[B\e[%dG", (term_size.ws_col / 2) - 12);
+		printf("\e[90m███\e[91m███\e[92m███\e[93m███\e[94m███\e[95m███\e[96m███\e[97m███");
+
+		printf("\e[B");
+		while (output_format[i]) {
+			printf("\e[B\e[%dG", (term_size.ws_col / 2) - 12);
+			printf(output_format[i], colour, output_value[i]);
+			i++;
+		}
+		printf("\n\n");
+	}
 
 	return 0;
 }
+
+//printf("\e[B\e[43G"); // Cursor position.
+/* TODO: make cursor position and text colour dependant on selected ascii art. */
+/*
+printf("\e[%s;1m%s\e[0m\e[B\e[43G%s\e[B\e[43G", colour, "Simplefetch", "~~~~~~~~~~~"); // Title.
+printf(field_format, colour, "Distro", "Arch Linux");
+printf(field_format, colour, "Window Manager", "dwl");
+printf(field_format, colour, "Packages", "? - pacman");
+printf(field_format, colour, "Terminal", "Alacritty");
+printf(field_format, colour, "Terminal Font", "Hack Nerd Font");
+printf(field_format, colour, "Shell", "bash 5.2.26");
+printf(field_format, colour, "CPU", "AMD Ryzen 3 3200G @ 3.6 GHz");
+printf(field_format, colour, "GPU", "AMD Radeon RX 6600");
+printf(field_format, colour, "Memory", "16 GiB");
+printf("\e[%s;1mStorage\e[0m: %d / %d GiB\e[53C\e[19D[%s] (%d%%)\e[B\e[43G", colour, used_storage, total_storage, storage_bar, storage_percent);
+
+printf("\e[B\e[43G");
+printf("\e[30m███\e[31m███\e[32m███\e[33m███\e[34m███\e[35m███\e[36m███\e[37m███\e[B\e[43G");
+printf("\e[90m███\e[91m███\e[92m███\e[93m███\e[94m███\e[95m███\e[96m███\e[97m███\e[B\e[43G");
+printf("\e[4E");
+*/
