368aede2fcbb02578e06bf32aec4019f7a560895
donthurtme1 <malwarecunt@proton.me>
2024-04-02 02:54:10 +0000

    Added uptime.

--- fetch.c
+++ fetch.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <sys/sysinfo.h>
 #include "ascii.h"
 
 static const char *ascii_distro = arch_ascii;
@@ -13,24 +14,24 @@ static const char simple_format[] = "\e[%s;1m%s\e[0m %s\e[B\e[43G";
 static char storage_bar[12] = "------------";
 
 static const char *output_format[] = {
-	"\e[%s;1m󰍹  \e[0m\e[90mDistro \t\e[0m%s",
-	"\e[%s;1m󱂬  \e[0m\e[90mWM \t\t\e[0m%s",
-	"\e[%s;1m  \e[0m\e[90mPackages \t\e[0m%s",
-	"\e[%s;1m  \e[0m\e[90mTerminal \t\e[0m%s",
-	"\e[%s;1m  \e[0m\e[90mUptime \t\e[0m%s",
-	"\e[%s;1m  \e[0m\e[90mBrowser \t\e[0m%s",
-	"\e[%s;1m  \e[0m\e[90mCPU \t\t\e[0m%s",
+	"\e[%s;1m󰍹  \e[0m\e[90mDistro    \e[0m%s",
+	"\e[%s;1m  \e[0m\e[90mPackages  \e[0m%s",
+	"\e[%s;1m  \e[0m\e[90mTerminal  \e[0m%s",
+	"\e[%s;1m  \e[0m\e[90mUptime    \e[0m%s",
+	"\e[%s;1m  \e[0m\e[90mBrowser   \e[0m%s",
+	"\e[%s;1m  \e[0m\e[90mCPU       \e[0m%s",
+	"\e[%s;1m  \e[0m\e[90mStorage   \e[%sm%d / %d GiB",
 	NULL
 };
 
 static const char *output_value[] = {
 	"Arch Linux",
-	"dwl",
-	"{packages}",
+	NULL,
 	"Alacritty",
-	"{uptime}",
+	NULL,
 	"Firefox",
 	"AMD Ryzen 3",
+	"0",
 	NULL
 };
 
@@ -80,13 +81,28 @@ int main(int argc, char *argv[]) {
 	struct winsize term_size;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &term_size);
 
+	/* Packages */
+
+	/* Uptime */
+	struct sysinfo sys_info;
+	char uptime_str[5] = "00:00";
+	sysinfo(&sys_info);
+	int upmins = (sys_info.uptime / 60) % 60;
+	int uphours = (sys_info.uptime / 3600) % 60;
+
+	uptime_str[4] = (upmins % 10) + '0';
+	uptime_str[3] = (upmins / 10) + '0';
+	uptime_str[1] = (uphours % 10) + '0';
+	uptime_str[0] = (uphours / 10) + '0';
+	output_value[3] = uptime_str;
+
 	/* Storage */
 	int total_storage, used_storage, storage_percent;
 	const char *fetch_cache_dir = concat(getenv("HOME"), "/.cache/fetch/");
 	const char *fetch_storage = concat(fetch_cache_dir, "storage");
 	
 	mkdir(fetch_cache_dir, S_IRWXU);
-	if (system(concat("df --total | grep total > ", fetch_storage)) >= 0) {
+	if (system(concat("df --total | grep home > ", fetch_storage)) >= 0) {
 		FILE *fstorage = fopen(fetch_storage, "r");
 		fscanf(fstorage, "%*s %d %d %*s %d", &total_storage, &used_storage, &storage_percent);
 		fclose(fstorage);
@@ -100,23 +116,27 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* Print stuff. */
-	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 	printf("\e[16A");
 	i = 0;
 	printf("\e[%s;1m", colour);
 	if (!vertical) {
-		printf("%s", space_invader2);
+		printf("\e[2B\e[8C%s", space_invader_offset);
+
+		printf("\e[3B\e[3G");
+		printf("\e[30m███\e[31m███\e[32m███\e[33m███\e[34m███\e[35m███\e[36m███\e[37m███");
+		printf("\e[B\e[3G");
+		printf("\e[90m███\e[91m███\e[92m███\e[93m███\e[94m███\e[95m███\e[96m███\e[97m███");
+
+		printf("\e[10A");
 		while (output_format[i]) {
-			printf("\e[B\e[28G");
-			printf(output_format[i], colour, output_value[i]);
+			printf("\e[B\e[30G");
+			printf(output_format[i], colour, output_value[i], used_storage, total_storage);
 			i++;
 		}
-		printf("\e[2B\e[28G");
-		printf("\e[30m███\e[31m███\e[32m███\e[33m███\e[34m███\e[35m███\e[36m███\e[37m███\e[B\e[28G");
-		printf("\e[90m███\e[91m███\e[92m███\e[93m███\e[94m███\e[95m███\e[96m███\e[97m███\e[B\e[28G");
-		printf("\e[E");
+		printf("\e[5E");
 	} else {
-		printf("\e[%dG%s", (term_size.ws_col / 2) - 6, space_invader2);
+		printf("\e[B\e[%dG%s", (term_size.ws_col / 2) - 6, space_invader_offset);
 
 		printf("\e[2B\e[%dG", (term_size.ws_col / 2) - 12);
 		printf("\e[30m███\e[31m███\e[32m███\e[33m███\e[34m███\e[35m███\e[36m███\e[37m███");
@@ -126,7 +146,7 @@ int main(int argc, char *argv[]) {
 		printf("\e[B");
 		while (output_format[i]) {
 			printf("\e[B\e[%dG", (term_size.ws_col / 2) - 12);
-			printf(output_format[i], colour, output_value[i]);
+			printf(output_format[i], colour, output_value[i], used_storage, total_storage);
 			i++;
 		}
 		printf("\n\n");
