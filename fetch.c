4a0ee8af7a3f5748eb5cdc6fb8dfacc3ec315881
donthurtme1 <malwarecunt@proton.me>
2024-06-20 15:56:13 +0100

    Fixed thing.

--- fetch.c
+++ fetch.c
@@ -160,24 +160,25 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-	printf("\e[16A");
+	printf("\e[15A");
 	i = 0;
 	printf("\e[%s;1m", colour);
 	if (!vertical) {
 		printf("\e[2B\e[8C%s", simple_artix);
 
-		printf("\e[3B\e[3G");
-		printf("\e[30m███\e[31m███\e[32m███\e[33m███\e[34m███\e[35m███\e[36m███\e[37m███");
-		printf("\e[B\e[3G");
-		printf("\e[90m███\e[91m███\e[92m███\e[93m███\e[94m███\e[95m███\e[96m███\e[97m███");
-
-		printf("\e[10A");
+		printf("\e[11A\e[31G");
 		while (output_format[i]) {
-			printf("\e[B\e[30G");
+			printf("\e[B\e[31G");
 			printf(output_format[i], colour, output_value[i], used_storage, total_storage);
 			i++;
 		}
-		printf("\e[5E");
+
+		printf("\e[2B\e[31G");
+		printf("\e[30m███\e[31m███\e[32m███\e[33m███\e[34m███\e[35m███\e[36m███\e[37m███");
+		printf("\e[B\e[31G");
+		printf("\e[90m███\e[91m███\e[92m███\e[93m███\e[94m███\e[95m███\e[96m███\e[97m███");
+
+		printf("\e[4E");
 	} else {
 		printf("\e[B\e[%dG%s", (term_size.ws_col / 2) - 6, space_invader_offset);
 
